aeaff58fcfe37a6737ac37d890fef2ac66ae921e
Justin Miller <jayman1294@gmail.com>
2019-11-22 20:08:55 +0000

    made changes to files

--- lib/project_cli/cli.rb
+++ lib/project_cli/cli.rb
@@ -2,5 +2,18 @@ class ProjectCli::CLI
   
   def call 
     puts "Would you like to know the weather?"
+    location
+    
   end
+  
+  def location
+    puts "Enter the location of where you would like to know the weather"
+    input = gets.strip
+    case input
+    when "New York"
+      puts "78 degrees"
+    when "Chicago"
+      puts "53 degrees"
+    end
+  end 
 end
